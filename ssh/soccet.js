9db63f2bb8954207ad7946cae50df60d193f49ac
pur3vessel <pur3vessel@mail.ru>
2021-11-06 19:11:02 +0300

    Тестируется websoccet соединение

--- ssh/soccet.js
+++ ssh/soccet.js
@@ -1,8 +1,35 @@
 window.addEventListener("load", function(){
-    var form = document.getElementById("form")
+    this.alert("load")
+  
+    var form2 = document.getElementById("form2")
+    connectBut = this.document.getElementById("connect")
+    connectBut.addEventListener("click", function() {
+        var socket = new WebSocket("ws://127.0.0.1:6060/ws");
+        socket.onopen = function() {
+            alert("Соединение установлено.");
+            document.getElementById("form-container2").style.display = "block"
+          };
+          
+          socket.onclose = function(event) {
+            if (event.wasClean) {
+              alert('Соединение закрыто чисто');
+            } else {
+              alert('Обрыв соединения'); // например, "убит" процесс сервера
+            }
+            alert('Код: ' + event.code + ' причина: ' + event.reason);
+          };
+          
+          socket.onmessage = function(event) {
+            alert("Получены данные " + event.data);
+          };
+          
+          socket.onerror = function(error) {
+            alert("Ошибка " + error.message);
+          };
+    })
     let command = document.getElementById("command")
     command.value = ""
-    form.addEventListener("submit", function(event){
+    form2.addEventListener("submit", function(event){
         event.preventDefault();
         let answer = document.getElementById("answer")
         answer.innerHTML = command.value
---
+++ ssh/websoccetTest.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait = 10 * time.Second
+
+	pongWait = 60 * time.Second
+
+	pingPeriod = (pongWait * 9) / 10
+
+	maxMessageSize = 512
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type Client struct {
+	conn *websocket.Conn
+
+	send chan []byte
+}
+
+func reverse(arr []byte) []byte {
+	if len(arr) == 0 {
+		return arr
+	}
+	return append(reverse(arr[1:]), arr[0])
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = reverse(message)
+		c.send <- message
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{conn: conn, send: make(chan []byte, 256)}
+
+	go client.writePump()
+	go client.readPump()
+}
+
+func main() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r)
+	})
+	err := http.ListenAndServe("127.0.0.1:6060", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
